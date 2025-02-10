f14f91d5ed442f7a99094f6eb8531f27ad8c6acc
Resqdeeo <vlazer03@mail.ru>
2025-02-10 23:33:20 +0300

    added

--- Backend/RockPaperScissors.Application/Abstractions/CQRS/ICommandDispatcher.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace RockPaperScissors.Application.Abstractions;
-
-public interface ICommandDispatcher
-{
-    Task<TResult> Send<TCommand, TResult>(TCommand command) where TCommand : ICommand<TResult>;
-}
