cfaa0b5bbcb1ff19a071ef8182172e042eae3163
Resqdeeo <vlazer03@mail.ru>
2025-02-10 23:29:30 +0300

    added auth, entities, dtos

---
+++ Backend/RockPaperScissors.Application/DTOs/Auth/Register/RegisterRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace RockPaperScissors.Application.DTOs.Auth.Register;
+
+public class RegisterRequest
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
