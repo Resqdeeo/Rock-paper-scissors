d8f9e6679652ff9c2cf0017eb56495be4ce1d46d
Resqdeeo <vlazer03@mail.ru>
2025-02-11 13:12:05 +0300

    add

---
+++ Backend/RockPaperScissors.Application/DTOs/Game/GetGames/GetGamesRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace RockPaperScissors.Application.DTOs.Game.GetGames;
+
+public class GetGamesRequest
+{
+    public int Page { get; set; }
+    public int Size { get; set; }
+}
---
+++ Backend/RockPaperScissors.Domain/Entities/UserRating.cs
@@ -0,0 +1,8 @@
+﻿namespace RockPaperScissors.Domain.Entities;
+
+public class UserRating
+{
+    public Guid UserId { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public int Rating { get; set; } = 0;
+}
